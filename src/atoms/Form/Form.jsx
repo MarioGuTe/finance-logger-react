42543cbbe37538ef335af8a42b3689c477b25dc3
MarioGuTe <mariogutierreztello@gmail.com>
2023-09-30 17:55:19 -0300

    add form logic

--- src/atoms/Form/Form.jsx
+++ src/atoms/Form/Form.jsx
@@ -1,25 +1,68 @@
-const Form = () => {
+import { useState } from "react";
+
+useState;
+const Form = ({ formData }) => {
+  const [formInfo, setFormInfo] = useState({
+    finance_type: "invoice",
+    to_from: "",
+    details: "",
+    amount: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    formData(formInfo);
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setFormInfo({ ...formInfo, [e.target.name]: value });
+  };
+
   return (
     <>
-      <form className="new-item-form">
-        <div class="field">
+      <form className="new-item-form" onSubmit={handleSubmit}>
+        <div className="field">
           <label>Type:</label>
-          <select id="type">
+          <select
+            id="type"
+            name="finance_type"
+            onChange={handleChange}
+            value={formInfo.finance_type}
+          >
             <option value="invoice">Invoice</option>
             <option value="payment">Payment</option>
           </select>
         </div>
-        <div class="field">
+        <div className="field">
           <label>To / From:</label>
-          <input type="text" id="tofrom" />
+          <input
+            type="text"
+            id="tofrom"
+            value={formInfo.to_from}
+            name="to_from"
+            onChange={handleChange}
+          />
         </div>
-        <div class="field">
+        <div className="field">
           <label>Details:</label>
-          <input type="text" id="details" />
+          <input
+            type="text"
+            id="details"
+            name="details"
+            value={formInfo.details}
+            onChange={handleChange}
+          />
         </div>
-        <div class="field">
+        <div className="field">
           <label>Amount ($):</label>
-          <input type="number" id="amount" />
+          <input
+            type="number"
+            id="amount"
+            name="amount"
+            value={formInfo.amount}
+            onChange={handleChange}
+          />
         </div>
         <button>Add</button>
       </form>
--- src/molecules/App.jsx
+++ src/molecules/App.jsx
@@ -1,16 +1,23 @@
+import { useState } from "react";
 import List from "../atoms/List/List";
 import Form from "../atoms/Form/Form";
 
 function App() {
+  const [newLog, setNewLog] = useState([]);
+
+  function updateLogInfo(formData) {
+    setNewLog(formData);
+    console.log("fired", newLog, "hello");
+  }
+
   return (
     <>
-      <div class="wrapper">
+      <div className="wrapper">
         <h1>Finance Logger</h1>
-
-        <List />
+        <List itemLog={newLog} />
       </div>
       <footer>
-        <Form />
+        <Form formData={updateLogInfo} />
       </footer>
     </>
   );
